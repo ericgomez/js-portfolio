c1cd84c5e8793b6772717d9c1b187f73efab5696
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-12 13:32:54 -0600

    Webpack en modo desarrollo

---
+++ webpack.config.dev.js
@@ -0,0 +1,97 @@
+const path = require('path');// Para trabajar con archivos y rutas de directorios
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+
+//añadir el autocompletado para nuestro archivo de webpack @type
+/** @type {import('webpack').Configuration} */ 
+module.exports = {
+  mode: 'development',
+   // Entry nos permite decir el punto de entrada de nuestra aplicación
+  entry: './src/index.js',
+  // Output nos permite decir hacia dónde va enviar lo que va a preparar webpacks
+  output: {
+    // path es donde estará la carpeta donde se guardará los archivos
+    // Con path.resolve podemos decir dónde va estar la carpeta y la ubicación del mismo
+    path: path.resolve(__dirname, 'dist'),
+    // filename le pone el nombre al archivo final
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
+  },
+  resolve: {
+    // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
+    extensions: ['.js'],
+    alias: {
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        // Test declara que extensión de archivos aplicara el loader
+        test: /\.m?js$/,
+        // Exclude permite omitir archivos o carpetas especificas
+        exclude: /(node_modules|bower_components)/,
+        // Use es un arreglo u objeto donde dices que loader aplicaras
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
+      },
+      {
+        test: /\.png/,
+        type: "asset/resource"
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            // limit => limite de tamaño
+            limit: 10000,
+            // Mimetype => tipo de dato
+            mimetype: "application/font-woff",
+            // name => nombre de salida
+            name: "[name].[contenthash].[ext]",
+            // outputPath => donde se va a guardar en la carpeta final
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        },
+      },
+    ]
+  },
+  plugins: [
+    //hacemos una instancia de lo que definimos en el inicio del archivo
+    // le anadimos por parametro un objeto donde vamos a tener las 
+    //configuraciones que le vamos anadir a nuestro plugin
+    new HtmlWebpackPlugin({
+      //inyecta el bundle al template html
+      inject: true,
+       //la ruta al template html
+      template: './public/index.html',
+      //nombre final del archivo
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "src", "assets/images"), to: "assets/images" },
+      ],
+    }),
+    new Dotenv(),
+  ],
+}
