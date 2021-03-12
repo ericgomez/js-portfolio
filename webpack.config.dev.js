b8f5cbe5fb8de05b7adc28d5596d7cb39beff4e3
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-12 15:37:57 -0600

    Webpack Watch

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -19,6 +19,8 @@ module.exports = {
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
+  // El modo watch hace que nuestro proyecto se compile de forma automática
+  watch: true,
   resolve: {
     // Aqui ponemos las extensiones que tendremos en nuestro proyecto para webpack los lea
     extensions: ['.js'],
